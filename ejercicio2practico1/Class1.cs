14553ca83334c7881828ff3a1a32a17ae01ac73b
marti <martinagoncalvezz@gmail.com>
2024-12-07 15:25:12 -0300

    Agregar archivos de proyecto.

---
+++ ejercicio2practico1/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2practico1
+{
+    internal class Alumno
+    {
+        private string nombre;
+        private int edad;
+        private string curso;
+
+        public string getnombre()
+        {
+            return nombre;
+        }
+
+        public int getedad() 
+        { 
+            return edad;
+        }
+
+        public string getcurso() 
+        {
+            return curso;
+        }
+
+        public void setnombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+
+        public void setedad(int edad) 
+        { 
+            this.edad = edad;
+        }
+
+        public void setcurso(string curso)
+        {
+            this.curso = curso;
+        }
+
+        public Alumno(string nombre, int edad, string curso)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            this.curso = curso;
+        }
+    }
+}
---
+++ ejercicio2practico1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio2practico1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno pepe = new Alumno("Pepe", 16, "A");
+            
+            Alumno marta = new Alumno("Marta", 17, "B");
+
+            Console.WriteLine($"Alumno 1: Nombre = {pepe.getnombre()}, Edad = {pepe.getedad()}, Curso = {pepe.getcurso()}");
+            Console.WriteLine($"Alumno 2: Nombre = {marta.getnombre()}, Edad = {marta.getedad()}, Curso = {marta.getcurso()}");
+
+        }
+    }
+}
